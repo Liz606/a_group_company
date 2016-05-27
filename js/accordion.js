6de1df08ef0ebd20169355815f509f986f927085
Liz606 <1105394023@qq.com>
2016-05-27 20:45:57 +0800

    update

--- js/accordion.js
+++ js/accordion.js
@@ -1,97 +1,171 @@
 accorInit('.newsWrap');
-function accorInit(_super) {
-	var _super=$(_super);
-	var _children=_super.children().filter('.new');
-	var dg_nextArr=_super.parent().children().filter('.dg-nextArr');
-	var dg_prevArr=_super.parent().children().filter('.dg-prevArr');
-	var dg_nextArrOn=_super.parent().children().filter('#blackMaskNew').children().filter('.dg-nextArr');
-	var dg_prevArrOn=_super.parent().children().filter('#blackMaskNew').children().filter('.dg-prevArr');
-	var tempDom;
-	$.each(_children,function() {
+function accorInit(_sup) {
+	var sup=$(_sup);
+     var W=$(window).width();
+	var children=sup.children().filter('.new');
+	var dg_nextArr=sup.parent().children().filter('.dg-nextArr');
+	var dg_prevArr=sup.parent().children().filter('.dg-prevArr');
+	var dg_nextArrOn=sup.parent().children().filter('#blackMaskNew').children().filter('.dg-nextArr');
+	var dg_prevArrOn=sup.parent().children().filter('#blackMaskNew').children().filter('.dg-prevArr');
+	$.each(children,function() {
 		$(this).click(function() {
+		//如果已经大图，直接显示遮罩，否则要移动、显示大图、显示遮罩
 			if ($(this).hasClass('newOpen')) {
-				$('#blackMaskNew').removeClass('none').addClass('block');
+				 $('#blackMaskNew').LizToggleOne('none','block');
+				newsGetFile($(this));
 			}else{
-				$('#blackMaskNew').removeClass('none').addClass('block');
 				changeOpen($(this));
+				 $('#blackMaskNew').LizToggleOne('none','block');
+				newsGetFile($(this));
 			}
 		});
 	});
 
 	dg_prevArr.click(function() {
-		_children=_super.children().filter('.new');
-		changeOpen(_children[_children.length-1]);
+		arr_l();
 	});
 	dg_nextArr.click(function() {
-		_children=_super.children().filter('.new');
-		changeOpen(_children[1]);
+		arr_r();
 	});
-
 	dg_prevArrOn.click(function() {
-		_children=_super.children().filter('.new');
-		_children[_children.length-1].click();
+		arr_l();
+		newsGetFile(children.filter('.newOpen'));
 	});
 	dg_nextArrOn.click(function() {
-		_children=_super.children().filter('.new');
-		_children[1].click();
+		arr_r();
+		newsGetFile(children.filter('.newOpen'));
 	});
-	function changeOpen($this) {
-		$this=$($this);
-		_super=$(_super);
-		_children=_super.children().filter('.new');
-		var $that=_children.filter('.newOpen');
-				$that.removeClass('newOpen');
-				$that.children().first().removeClass('newImgOpen').addClass('newImg');
-				$that.children().last().removeClass('newTextOpen').addClass('newText');
-				$that.children().last().children().eq(0).removeClass('newTitleOpen').addClass('newTitle');
-				$that.children().last().children().eq(1).removeClass('newDateOpen').addClass('newDate');
-				$that.children().last().children().eq(2).removeClass('block').addClass('none');
-			toLast($that);
-			if ($this!=_children[1]) {
-				toLast($this.prev());
-			};
-			$this.addClass('newOpen');
-			$this.children().first().removeClass('newImg').addClass('newImgOpen');
-			$this.children().last().removeClass('newText').addClass('newTextOpen');
-			$this.children().last().children().eq(0).removeClass('newTitle').addClass('newTitleOpen');
-			$this.children().last().children().eq(1).removeClass('newDate').addClass('newDateOpen');
-			$this.children().last().children().eq(2).removeClass('none').addClass('block'); 
-			var title,date,content,bgurl;
-			bgurl=$this.children().first().css('background-image');
-			console.log(navigator.userAgent);
-			var userAgent=navigator.userAgent;
-			if (userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1) {
-				bgurl=bgurl.substring(4,bgurl.length-1);
-			}else if ( userAgent.indexOf("Edge") > -1) {
-				bgurl=bgurl.substring(4,bgurl.length-1);
-			}else if (true){
-				bgurl=bgurl.substring(5,bgurl.length-2);
-			};
-			title=$this.children().last().children().eq(0).text();
-			date=$this.children().last().children().eq(1).text();
-			content=$this.children().last().children().eq(2).text();
-			$('.newTitleOpen').text('').text(title);
-			$('.lookTitle').text('').text(title);
-			$('.newDateOpen').text('').text(date);
-			$('.newContentBig').text('').text(content);
-			$('.newimgBig').attr('src',bgurl);
+	function arr_l() {
+		//dom移动
+		
+		var prev=children.filter('.newOpen').removeClass('n1').addClass('n2');
+		if (prev.attr('index')==1) {
+			now=children.eq(children.length-1);
+			toggleStyle(prev,now);
+			now.addClass('n1').LizToggleOne('hidden','visible');
+			prev.next().removeClass('n2').addClass('n3');
+			prev.next().next().removeClass('n3').LizToggleOne('visible','hidden');
+		}else if (prev.attr('index')==children.length) {
+			now=prev.prev();
+			now.addClass('n1').LizToggleOne('hidden','visible');
+			toggleStyle(prev,now);
+			children.eq(0).removeClass('n2').addClass('n3');
+			children.eq(1).removeClass('n3').LizToggleOne('visible','hidden');
+		}else if (prev.attr('index')==children.length-1) {
+			now=prev.prev();
+			now.addClass('n1').LizToggleOne('hidden','visible');
+			toggleStyle(prev,now);
+			prev.next().removeClass('n2').addClass('n3');
+			children.eq(0).removeClass('n3').LizToggleOne('visible','hidden');
+		}else{
+			prev.next().removeClass('n2').addClass('n3');
+			prev.next().next().removeClass('n3').LizToggleOne('visible','hidden');
+			var now=prev.prev();
+			now.addClass('n1').LizToggleOne('hidden','visible');
+			toggleStyle(prev,now);
+		}
+	
 	};
-	//移动dom至末尾
-	function toLast($that) {
-		var tempDom=$that.clone();
-			tempDom.click(function() {
-				if ($(this).hasClass('newOpen')) {
-					$('#blackMaskNew').removeClass('none').addClass('block');
-				}else{
-					$('#blackMaskNew').removeClass('none').addClass('block');
-					changeOpen($(this));
-				}
-			});
-			tempDom.appendTo(_super);	
-			$that.remove();
+	function arr_r() {
+		//dom移动
+			var prev=children.filter('.newOpen');
+			prev.removeClass('n1').LizToggleOne('visible','hidden');
+		if (prev.attr('index')==children.length-2) {
+			now=prev.next();
+			now.addClass('n1');
+			toggleStyle(prev,now);
+			now.next().removeClass('n3').addClass('n2');
+			children.eq(0).addClass('n3').LizToggleOne('hidden','visible');
+		}else if (prev.attr('index')==children.length) {
+			now=children.eq(0);
+			now.removeClass('n2').addClass('n1');
+			now.next().removeClass('n3').addClass('n2');
+			toggleStyle(prev,now);
+			children.eq(2).addClass('n3').LizToggleOne('hidden','visible');
+		}else if (prev.attr('index')==children.length-1) {
+			now=prev.next();
+			now.addClass('n1');
+			toggleStyle(prev,now);
+			children.eq(0).removeClass('n3').addClass('n2');
+			children.eq(1).addClass('n3').LizToggleOne('hidden','visible');
+		}else{
+			var now=prev.next();
+			now.removeClass('n2').addClass('n1');
+			now.next().removeClass('n3').addClass('n2');
+			now.next().next().addClass('n3').LizToggleOne('hidden','visible');
+			toggleStyle(prev,now);
+		}
 	};
-	$('#maskXNew').click(function() {
-		$('#blackMaskNew').addClass('none').removeClass('block');
-	});
+	//异步得到详情数据
+	function newsGetFile($this) {
+		//清除数据；
+		//获取数据；
+		//插入数据；
+		 $('#lookOnlyNew>.newTitleOpen').text();
+		 $('#lookOnlyNew>.newDataOpen').text();
+		 $('#lookOnlyNew>.boxScorll').children().remove();
+		 $('#lookOnlyNew').next().text();
+		    $.ajax({
+		       type: "GET",
+		       url: $this.data("url"),
+		       dataType: "json",
+		       success: function(data){
+		         console.log( "Data Saved: " + data);
+		 			$('#lookOnlyNew>.newTitleOpen').text(data.title);
+		 			$('#lookOnlyNew').next().text(data.title);
+		            $('#lookOnlyNew>.newDataOpen').text(data.date);
+		            $('#lookOnlyNew>.boxScorll').append(unescape(data.content));
+		           },
+		           err:function() {
+		                console.log( "出错啦~~~~" );
+		           }
+		       })
+	};
+	//改变样式
+	function toggleStyle(prev,now) {
+		var prevC=prev.children();
+		var prevLC=prevC.eq(1).children();
+		var nowC=now.children();
+		var nowLC=nowC.eq(1).children();
+		prev.removeClass('newOpen');
+		prevC.eq(0).LizToggleOne('newImgOpen','newImg');
+		prevC.eq(1).LizToggleOne('newTextOpen','newText');
+		prevLC.eq(0).LizToggleOne('newTitleOpen','newTitle');
+		prevLC.eq(1).LizToggleOne('newDateOpen','newDate');
+		prevLC.eq(2).LizToggleOne('block','none');
 
+		now.addClass('newOpen');
+		nowC.eq(0).LizToggleOne('newImg','newImgOpen');
+		nowC.eq(1).LizToggleOne('newText','newTextOpen');
+		nowLC.eq(0).LizToggleOne('newTitle','newTitleOpen');
+		nowLC.eq(1).LizToggleOne('newDate','newDateOpen');
+		nowLC.eq(2).LizToggleOne('none','block'); 
+	};
+	//直接点击小图产生的dom移动
+	function changeOpen(obj) {
+		 var _prev=children.filter('.newOpen');
+		 var _now=obj;
+		 var _prevIndex=parseInt(_prev.attr('index'));
+		 var _nowIndex=parseInt(_now.attr('index'));
+		 if (_nowIndex-_prevIndex==1) {
+		 	arr_r();
+		 }else if (_nowIndex-_prevIndex==2) {
+		 	arr_r();
+		 	arr_r();
+		 }else if (_nowIndex-_prevIndex==0) {
+		 	return;
+		 }
+	};
+	//关闭详情页
+	$('#maskXNew').click(function() {
+		$('#blackMaskNew').LizToggleOne('block','none');
+	});
+	//拓展$.prototype
+	$.fn.extend({
+		LizToggleOne:function(str1,str2) {
+			if ($(this).hasClass(str1)) {
+				$(this).removeClass(str1).addClass(str2);
+			}
+		}
+	});
 }
--- js/divselect.js
+++ js/divselect.js
@@ -1,10 +1,9 @@
 (function($) {
 	divselect("#divselect1","#divselect2","#divselect3");
 	function divselect(divselectid1,divselectid2,divselectid3) {
+
 			var divSelects=[];
-			var logoLeft=$('.logoLeft');
-		    var logoRight=$('.logoRight');
-		    var menu=$('.menu');
+			
 			for (var i = 0; i < arguments.length; i++) {
 				divSelects.push($(arguments[i]+' cite'));
 			};			
@@ -28,57 +27,26 @@
 
 					var cite=$that.attr('selectid');//记录选中a的id ---1-2
 					var value=$this.attr('selectid');//已经存在的id ---1-3
-					if (cite==value) {
-						$this.parent().children('ul').removeClass('block').addClass('none');
-					}else{
 						var text=$that.text();
 						$this.html(text).attr('selectid',cite);//更新cite
 						$this.parent().children('ul').removeClass('block').addClass('none');//收起下拉框
 						var citeArr=cite.split('-');// 1 ， 2
 						setA(citeArr);
-						var thatNum=$this.parent().attr('id');
-						//显示···
-						if ($("."+citeArr.join('-')).length>0 && thatNum.substring(thatNum.length-1)==3) {
-							$("."+citeArr.join('-')).removeClass('none').addClass('block').addClass('roll-f2');
-							$('.store1').addClass('none').removeClass('block');
-							$('#storeHome>.home').trigger('3Drunstate');
-							$('.storeList').removeClass('block').addClass('none');
-							$('.storeClose').unbind( "click" );
-							logoLeft.removeClass('block').addClass('none');
-							logoRight.removeClass('block').addClass('none');
-							menu.removeClass('block').addClass('none');
-							$('.storeClose').click(function(){
-									$('.store1').addClass('block').removeClass('none');
-							        $("."+citeArr.join('-')).removeClass('block').removeClass('roll-f2').addClass('roll-f').addClass('none');
-							        $('.storeList').removeClass('none');
-							        $('#storeHome>.home').trigger('3Dstopstate');
-							        logoLeft.removeClass('none').addClass('block');
-									logoRight.removeClass('none').addClass('block');
-									menu.removeClass('none').addClass('block');
-							    })
-						};
-						function setA(arrfouse) {
+						function setA(arrfouse) {//选择了省自动更新市区···
 							console.log(divSelects.length);
 							if (citeArr.length<=divSelects.length) {
 								for (var i = citeArr.length - 1; i > citeArr.length - 2; i--) {
-									var atext = divSelects[i].parent()
-															.children('ul')
-															.filter(function() {
-																return $(this).attr('selectUlId')==citeArr.join('-');
-															}).children()
-															.eq(0)
-															.children()
-															.text();
 									citeArr.push('1');
-									divSelects[i].html(atext).attr('selectid',citeArr.join('-'));
+									divSelects[i].html('请选择').attr('selectid',citeArr.join('-'));
 									setA(citeArr);
 								};
-							};
+							}
 						};
-					}
+						   
+					});
 					
 				});
 			})//each---A
-	    })//each---cite
-	}//function
+	    }//each---cite
+	//function
 })(jQuery)
